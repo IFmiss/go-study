d503e144f18d2c3ba77c9292d22cd048e474af17
DavidDai <185098535@qq.com>
2018-10-19 15:19:11 +0800

    匿名函数的学习

--- src/funcquote/index.go
+++ src/funcquote/index.go
@@ -7,6 +7,14 @@ func swap(x, y *int) {
 }
 
 func TestQuote() {
+	add := func(x, y int) {
+		println(&x, " + ", &y, " = ", x + y)
+	}
+
+	calc := func(x, y int) int {
+		return x + y
+	}
+
 	var h1 int = 10
 	var h2 int = 20
 	println("引用传递值之前 h1：", h1)
@@ -16,4 +24,26 @@ func TestQuote() {
 
 	println("引用传递值之后 h1：", h1)
 	println("引用传递值之后 h2：", h2)
+
+	add(1, 2)
+
+	println("x + y = ", calc(222, 312))
+
+	firstNum := BiBao()
+	println("匿名函数firstNum第一次执行:", firstNum())
+	println("匿名函数firstNum第二次执行:", firstNum())
+	println("匿名函数firstNum第三次执行:", firstNum())
+
+	secondNum := BiBao()
+	println("匿名函数secondNum第一次执行:", secondNum())
+	println("匿名函数secondNum第二次执行:", secondNum())
+	println("匿名函数secondNum第三次执行:", secondNum())
+}
+
+func BiBao() func() int {
+	i := 0
+	return func() int {
+		i++
+		return i
+	}
 }
